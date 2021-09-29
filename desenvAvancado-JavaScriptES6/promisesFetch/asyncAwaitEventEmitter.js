a0bdf783534bf77d3a6b49a83e4ef4538b24630d
Rodrigo Ide <rodriigo.ide@gmail.com>
2021-09-29 17:06:34 -0300

    funcao async e await

---
+++ desenvAvancado-JavaScriptES6/promisesFetch/asyncAwaitEventEmitter.js
@@ -0,0 +1,18 @@
+// ES7 - Async / Await
+
+const asyncTimer = () => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(12345);
+    }, 1000);
+});
+
+const simpleFunc = async() => {
+    const data = await asyncTimer();
+    return data;
+}
+
+simpleFunc().then(data =>{
+    console.log(data);
+}).catch(err =>{
+    console.log(err);
+});
