df1e930dd2242758f529dc7f4637fa828a9361e8
Rodrigo Ide <rodriigo.ide@gmail.com>
2021-09-30 15:36:43 -0300

    desenvolvendo codigos com sinon

--- desenvAvancado-JavaScriptES6/codequality-and-testautomation/test/math.spec.js
+++ desenvAvancado-JavaScriptES6/codequality-and-testautomation/test/math.spec.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const Math = require('../src/math.js');
 const expect = require('chai').expect;
+const sinon = require('sinon');
 
 let value = 0
 
@@ -23,7 +24,7 @@ describe('Math class', function(){
         })
     });
 
-    it.only('Multiply two numbers', function(){
+    it('Multiply two numbers', function(){
         const math = new Math();
         const obj = {
             name: 'Rodrigo Ide'
@@ -37,5 +38,25 @@ describe('Math class', function(){
         // expect(math.multiply(value, 5)).to.equal(0);
         // expect(obj).to.have.property('name').equal('Rodrigo Ide');
         expect(obj).to.deep.equal(obj2)
+    });
+
+    it.only('Calls res with sum and index values', function(){
+        const req = {};
+        const res = {
+            load: function load(){
+                console.log('Called!');
+            }
+        };
+
+        //sinon.spy(res, 'load');
+        sinon.stub(res, 'load').returns('xpto');
+        const math = new Math();
+
+        math.printSum(req, res, 5, 5);
+        
+        // expect(res.load.calledOnce).to.be.true;
+        // expect(res.load.args[0][0]).to.equal('index');
+        expect(res.load.args[0][1]).to.equal(10);
+
     });
 });
