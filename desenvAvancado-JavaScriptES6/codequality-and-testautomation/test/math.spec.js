6b40e239c329bdacedbc9a327561bcf808aa3ea4
Rodrigo Ide <rodriigo.ide@gmail.com>
2021-09-30 14:37:39 -0300

    funcionamento da biblioteca chai

--- desenvAvancado-JavaScriptES6/codequality-and-testautomation/test/math.spec.js
+++ desenvAvancado-JavaScriptES6/codequality-and-testautomation/test/math.spec.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const Math = require('../src/math.js');
+const expect = require('chai').expect;
 
 let value = 0
 
@@ -16,14 +17,25 @@ describe('Math class', function(){
         value = 5;
 
         math.sum(value, 5, (value) => {
-            assert.equal(value, 10);
+            //assert.equal(value, 10);
+            expect(value).to.equal(10);
             done();
         })
     });
 
-    it('Multiply two numbers', function(){
+    it.only('Multiply two numbers', function(){
         const math = new Math();
+        const obj = {
+            name: 'Rodrigo Ide'
+        };
         
-        assert.equal(math.multiply(value, 5), 0);
+        const obj2 = {
+            name: 'Rodrigo Ide'
+        }
+
+        // assert.equal(math.multiply(value, 5), 0);
+        // expect(math.multiply(value, 5)).to.equal(0);
+        // expect(obj).to.have.property('name').equal('Rodrigo Ide');
+        expect(obj).to.deep.equal(obj2)
     });
 });
