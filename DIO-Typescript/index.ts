b4209b9ee3964d01a7a92cef092a57a35731dc44
Rodrigo Ide <rodriigo.ide@gmail.com>
2021-10-14 10:35:02 -0300

    como funcionam as interfaces

--- DIO-Typescript/index.ts
+++ DIO-Typescript/index.ts
@@ -1,3 +1,27 @@
-function soma(a: number, b: number){
-    return a + b;
+// types
+// interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: "terrestre" | "aquático";
+    executarRugido(alturaEmDecibeis: number): void;
+}
+
+interface IFelino extends IAnimal{
+    visaoNortuna: boolean;
+}
+
+const animal: IAnimal = {
+    nome: "elefante",
+    tipo: "terrestre",
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+}
+
+console.log(animal.executarRugido(50));
+
+const felino: IFelino = {
+    nome: "leão",
+    tipo: "terrestre",
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`),
+    visaoNortuna: true
 }
