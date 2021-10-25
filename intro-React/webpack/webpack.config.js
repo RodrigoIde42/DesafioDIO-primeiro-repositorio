aa92edb2ee8a9b0cf25be3c1eac127cc2d64a60f
Rodrigo Ide <rodriigo.ide@gmail.com>
2021-10-25 12:19:37 -0300

    Installing and configuring ESLint

--- intro-React/webpack/webpack.config.js
+++ intro-React/webpack/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devtool: 'source-map',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
