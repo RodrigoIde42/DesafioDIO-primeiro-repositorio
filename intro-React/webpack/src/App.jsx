c72bbdcbae68a3df2878cde9a83272e1c21e8c7a
Rodrigo Ide <rodriigo.ide@gmail.com>
2021-10-25 15:27:36 -0300

    Arrays and keys

--- intro-React/webpack/src/App.jsx
+++ intro-React/webpack/src/App.jsx
@@ -3,30 +3,66 @@ import React from 'react';
 const ButtonA = <button>Customers history</button>
 const ButtonB = <button>Sign up customer</button>
 
-const hasCustomer = false;
+const hasCustomer = true;
+
+const customer = [
+    {
+        id: 1,
+        name: 'Rodrigo Ide',
+        skills: ['React', 'Node', 'CSS', 'Webpack']
+    },
+    {
+        id: 2,
+        name: 'Juan Perez',
+        skills: ['HTML', 'React Native', 'Go', 'JS']
+    },
+    {
+        id: 3,
+        name: 'Maria Rodriguez',
+        skills: ['Assembly']
+    },
+    {
+        id: 4,
+        name: 'Pedro Martinez',
+        skills: ['Reason']
+    }
+]
 
 const App = () => {
 
-    const renderShowHistory = () => (
+    const renderShowHistory = (
         <div>
             <p>Click the button below to show customers history</p>
             {ButtonA}
         </div>
     )
 
-    const renderSignUp = () => (
+    const renderSignUp = (
         <div>
             <p>Click the button below to sign up a new customer</p>
             {ButtonB}
         </div>
     )
 
-    const renderCustomer = () => {
-        if (!hasCustomer) return null
+    const renderCustomers = (customer) => {
+        if (hasCustomer) {
+            return (
+                <div key={`customer-${customer.id}`}>
+                    <li>
+                        {customer.name}
+                    </li>
+                    {customer.skills.map(renderSkills)}
+                </div>
+            )
+        }
+    }
 
+    const renderSkills = (skill, index) => {
         return (
-            <div>
-                <h1>Customer's name: Rodrigo Ide</h1>
+            <div style={{paddingLeft: '30px'}} key={`skill-${index}`}>
+                <li>
+                    {skill}
+                </li>
             </div>
         )
     }
@@ -36,10 +72,10 @@ const App = () => {
             <h1>Digital Innovation One</h1>
             <p>Conditional Rendering</p>
             <br/>
-            {hasCustomer ? renderShowHistory() : renderSignUp()}
+            {hasCustomer ? renderShowHistory : renderSignUp}
             <br/>
             <div>
-                {renderCustomer()}
+                    {customer.map(renderCustomers)}
             </div>
         </div>
     );
