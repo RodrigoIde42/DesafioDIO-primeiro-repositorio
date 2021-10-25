fe2616b085c08462e6347059d8f0875bbfc96d6b
Rodrigo Ide <rodriigo.ide@gmail.com>
2021-10-25 12:04:37 -0300

    Configuring the Webpack

---
+++ intro-React/webpack/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './Button';
+
+const App = () => {
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Test</p>
+            <Button text='Hello :D'/>
+        </div>
+    );
+};
+
+export default App;
---
+++ intro-React/webpack/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+};
