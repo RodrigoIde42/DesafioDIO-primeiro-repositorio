cab9c3aa25d34585808182cc02b0b8e6f73b5546
Rodrigo Ide <rodriigo.ide@gmail.com>
2021-10-26 16:24:01 -0300

    Importing a component

--- reactComponents/react-dio/src/App.js
+++ reactComponents/react-dio/src/App.js
@@ -1,17 +1,13 @@
+import Item from './components/Item/index'
+
 const App = () => {
   return (
     <>
       <h1>My first react application</h1>
       <ul>
-        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
-          Item 1
-        </a>
-        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
-          Item 2
-        </a>
-        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
-          Item 3
-        </a>
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
       </ul>
     </>
   )
