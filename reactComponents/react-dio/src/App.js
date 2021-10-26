a9f302cb7f71244976e36a431b9c1c4958ebbf9e
Rodrigo Ide <rodriigo.ide@gmail.com>
2021-10-26 16:21:58 -0300

    First steps in a component structure

--- reactComponents/react-dio/src/App.js
+++ reactComponents/react-dio/src/App.js
@@ -1,6 +1,19 @@
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>My first react application</h1>
+      <ul>
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
+          Item 1
+        </a>
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
+          Item 2
+        </a>
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
+          Item 3
+        </a>
+      </ul>
+    </>
   )
 }
 
