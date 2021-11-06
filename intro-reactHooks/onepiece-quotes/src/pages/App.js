63191d4a554e3aca2734000ff783b5847b7c15f5
Rodrigo Ide <rodriigo.ide@gmail.com>
2021-11-06 14:44:34 -0300

    finishing project

--- intro-reactHooks/onepiece-quotes/src/pages/App.js
+++ intro-reactHooks/onepiece-quotes/src/pages/App.js
@@ -1,18 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import chopperImg from '../images/chopper.png';
+import roomShamblesSound from '../sounds/Room_Shambles.mp3';
 
 import Quotes from '../components/Quotes/Quotes';
 import { getQuotes } from '../services/QuotesService/QuotesService';
 
-export default function App() {
+const audio = new Audio(roomShamblesSound);
 
-  const [quotes, setQuotes] = useState({ quote: 'quote', author: 'random author' });
+export default function App() {
+  let isMounted = useRef(true);
+
+  const [quotes, setQuotes] = useState({
+     quote: 'Loading quote...', 
+     author: 'Loading author...' 
+    });
 
   const onUpdate = async () => {
     const quote = await getQuotes();
-    setQuotes(quote);
+    audio.volume = 0.05;
+    if(isMounted.current) {
+      audio.play();
+
+      setTimeout(() => {
+        setQuotes(quote);
+      }, 2800);
+    }
   }
+
+  useEffect(() => {
+    setTimeout(() => {
+      onUpdate();
+    }, 500);
+
+    return () => {
+      isMounted.current = false;
+    }
+  }, [])
 
   return (
     <Content>
--- intro-reactHooks/onepiece-quotes/src/pages/App.test.js
+++ intro-reactHooks/onepiece-quotes/src/pages/App.test.js
@@ -8,18 +8,18 @@ const response = { author: 'Author', quote: 'Quote test' };
 
 const server = setupServer(
     rest.get(process.env.REACT_APP_API, (req, res, ctx) => {
-        return res(ctx.json(response));
+      return res(ctx.json(response));
     })
-)
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+  );
+  
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
 
 test('renders the app with a quote, an author, a button and an image', () => {
     render(<App />);
 
-    const textEl = screen.getByText(/random author/);
+    const textEl = screen.getByText(/Loading author/);
     const buttonEl = screen.getByRole('button');
     const imageEl = screen.getByRole('img');
 
@@ -29,15 +29,32 @@ test('renders the app with a quote, an author, a button and an image', () => {
 });
 
 test('calls an API on button click then update its text', async () => {
+    const customResponse = {
+        author: 'custom test author',
+        quote: 'test quote'
+    };
+
     render(<App />);
+
+    server.use(
+    rest.get(process.env.REACT_APP_API, (req, res, ctx) => {
+        return res(ctx.json(customResponse));
+    })
+    );
 
     const buttonEl = screen.getByRole('button');
 
     fireEvent.click(buttonEl);
 
-    const quoteEl = await screen.findByText(response.quote);
-    const authorEl = await screen.findByText(`- ${response.author}`);
+    const quoteEl = await screen.findByText(customResponse.quote);
 
     expect(quoteEl).toBeInTheDocument();
-    expect(authorEl).toBeInTheDocument();
+})
+
+test('calls an API on startup then renders response', async () => {
+    render(<App />);
+
+    const quoteEl = await screen.findByText(response.quote);
+
+    expect(quoteEl).toBeInTheDocument();
 })
