beaef4465615b14aa7271bc948f6fedacf78809b
RodrigoIde42 <rodriigo.ide@gmail.com>
2021-11-26 22:18:45 -0300

    finishing CRUD for user and creating a status route

--- rest-with-nodejs/src/index.ts
+++ rest-with-nodejs/src/index.ts
@@ -1,13 +1,14 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
+import statusRoute from "./routes/status.route";
 import usersRoute from "./routes/users.route";
 
 const app = express();
 
-app.use(usersRoute);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Success!!' });
-});
+app.use(usersRoute);
+app.use(statusRoute);
 
 app.listen(3000, () => {
     console.log('app started at port 3000!');
